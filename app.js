f8a8c7066deef9b772101c71ab8c23b823c92bf3
nshutinicolas <50362644+nshutinicolas@users.noreply.github.com>
2020-09-08 22:11:51 +0200

    correct file

--- app.js
+++ app.js
@@ -1,26 +1,16 @@
 const express = require("express");
 const app = express();
-const port = 3000;
-const { Client } = require("pg");
-// const Sequelize = require('sequelize');
-// const sequelize = new Sequelize("database", "mbrother", "12233445", {
-//   host: "localhost",
-//   dialect: "postgres",
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     idle: 10000,
-//   },
-// });
+const routes = require("./api/routes/routes");
+const cors = require("cors");
+const PORT = process.env.PORT || 2000;
+app.use(express.json());
+app.use(cors());
+require("dotenv").config();
 
-// sequelize.define("user", {
-//   firstname: { type: Sequelize.STRING },
-// });
-
-app.get("/", (req, res) => {
-  res.send("home page");
+// API ROUTES
+app.use("/api/v1/", routes);
+app.get("/test", (req, res) => {
+  res.send(process.env.DB_HOST);
 });
+app.listen(PORT, () => console.log("port up..."));
 
-app.listen(port, () => {
-  console.log("connected");
-});
